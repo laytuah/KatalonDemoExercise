871103e4efbf000572d0b764402e0f7e217585d0
Abdlateef <mightylaythuah@hotmail.com>
2023-07-17 20:04:33 +0100

    Test Report Implemented

--- KatalonDemoExercise/Hooks/Context.cs
+++ KatalonDemoExercise/Hooks/Context.cs
@@ -22,6 +22,15 @@ namespace KatalonDemoExercise.Hooks
             driver.Manage().Window.FullScreen();
         }
 
+        public void TakeScreenshotAtThePointOfTestFailure(string directory, string scenarioName)
+        {
+            Screenshot screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+            string path = directory + scenarioName + DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss") + ".png";
+            string Screenshot = screenshot.AsBase64EncodedString;
+            byte[] screenshotAsByteArray = screenshot.AsByteArray;
+            screenshot.SaveAsFile(path, ScreenshotImageFormat.Png);
+        }
+
         public void CloseKatalonDemoApplication()
         {
             driver.Quit();
--- KatalonDemoExercise/StepDefinitions/ViewCartSteps.cs
+++ KatalonDemoExercise/StepDefinitions/ViewCartSteps.cs
@@ -3,6 +3,9 @@ using TechTalk.SpecFlow;
 using KatalonDemoExercise.Hooks;
 using KatalonDemoExercise.Pages;
 using NUnit.Framework;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
 
 namespace KatalonDemoExercise.StepDefinitions
 {
@@ -12,14 +15,20 @@ namespace KatalonDemoExercise.StepDefinitions
         Context context;
         Homepage homepage;
         CartPage cartpage;
-        public ViewCartSteps(Context _context, Homepage _homepage, CartPage _cartpage)
+        ScenarioContext scenarioContext;
+        static ExtentTest feature;
+        static ExtentTest scenario;
+        static ExtentReports report;
+
+        public ViewCartSteps(Context _context, Homepage _homepage, CartPage _cartpage, ScenarioContext _scenarioContext)
         {
-            context = _context; homepage = _homepage; cartpage = _cartpage;
+            context = _context; homepage = _homepage; cartpage = _cartpage; scenarioContext = _scenarioContext;
         }
         [Given(@"that katalon Ecommerce website is loaded")]
         public void GivenThatKatalonEcommerceWebsiteIsLoaded()
         {
             context.LoadKatalonDemoApplication();
+            scenario = feature.CreateNode<Scenario>(scenarioContext.ScenarioInfo.Title);
         }
         
         [When(@"a user adds (.*) items to cart")]
@@ -47,10 +56,84 @@ namespace KatalonDemoExercise.StepDefinitions
             Assert.IsTrue(expectedNumberOfItemsInCart.Equals(actualNumberOfItemsInCart));
         }
 
-        [AfterScenario]
-        public void DiscardBrowser()
+        [BeforeTestRun]
+        public static void ReportGenerator()
         {
-            context.CloseKatalonDemoApplication();
+            var testResultReport = new ExtentV3HtmlReporter(AppDomain.CurrentDomain.BaseDirectory + @"\TestResult.html");
+            testResultReport.Config.Theme = AventStack.ExtentReports.Reporter.Configuration.Theme.Dark;
+            report = new ExtentReports();
+            report.AttachReporter(testResultReport);
+        }
+
+        [AfterTestRun]
+        public static void ReportCleaner()
+        {
+            report.Flush();
+        }
+
+        [BeforeFeature]
+        public static void BeforeFeature(FeatureContext featureContext)
+        {
+            feature = report.CreateTest<Feature>(featureContext.FeatureInfo.Title);
+        }
+
+        [AfterStep]
+        public void StepsInTheReport()
+        {
+            var typeOfStep = ScenarioStepContext.Current.StepInfo.StepDefinitionType.ToString();
+            if (scenarioContext.TestError == null)
+            {
+                if (typeOfStep.Equals("Given"))
+                {
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text);
+                }
+                else if (typeOfStep.Equals("When"))
+                {
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text);
+                }
+                else if (typeOfStep.Equals("Then"))
+                {
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text);
+                }
+            }
+
+            if (scenarioContext.TestError != null)
+            {
+                if (typeOfStep.Equals("Given"))
+                {
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text).Fail(scenarioContext.TestError.Message);
+                }
+                else if (typeOfStep.Equals("When"))
+                {
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text).Fail(scenarioContext.TestError.Message);
+                }
+                else if (typeOfStep.Equals("Then"))
+                {
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text).Fail(scenarioContext.TestError.Message);
+                }
+            }
+        }
+
+        [AfterScenario]
+        public void CloseTFLApplication()
+        {
+            try
+            {
+                if (scenarioContext.TestError != null)
+                {
+                    string scenarioName = scenarioContext.ScenarioInfo.Title;
+                    string directory = AppDomain.CurrentDomain.BaseDirectory + @"\ReportScreenshots\";
+                    context.TakeScreenshotAtThePointOfTestFailure(directory, scenarioName);
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                context.CloseKatalonDemoApplication();
+            }
         }
     }
 }
